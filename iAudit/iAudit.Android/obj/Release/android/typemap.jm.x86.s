96c1034b4d818c6fe240b88ba264b261064ba5d3
Harsh Aggarwal <officialharshagg8@gmail.com>
2019-09-27 23:44:36 -0500

    Do over, Native to Xamarin.Forms app

---
+++ iAudit/iAudit.Android/obj/Release/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 53cde1b3f6e3c451af3d9bd9439767b8cad49990 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	760
+	/* entry-length */
+	.long	232
+	/* value-offset */
+	.long	102
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 176321
+	.include	"typemap.jm.inc"
