30704d71b51ce941d7bd1e8408f92b5e62ab73fd
Sonia Lopez <sonlopez99@gmail.com>
2019-09-30 00:21:56 -0500

    months
    added months as list

--- iAudit/iAudit.Android/obj/Release/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ iAudit/iAudit.Android/obj/Release/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_DEBUG", "gen-compact-seq-points",
-		"XAMARIN_BUILD_ID", "a593c8bb-2904-4602-b175-9c468db9a0e4",
+		"XAMARIN_BUILD_ID", "e36a1d03-33a2-4954-b590-5598295c1b91",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
