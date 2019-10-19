672d8b2439d933518e2728c662fd81a60090cd35
harshaggarwal8 <officialharshagg8@gmail.com>
2019-10-19 16:27:08 -0500

    Splash screen, iOS added

--- iAudit/iAudit.Android/obj/Release/android/environment.arm64-v8a.s
+++ iAudit/iAudit.Android/obj/Release/android/environment.arm64-v8a.s
@@ -63,7 +63,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"8ba2de86-b47f-4c07-9c23-8d54089e29fe"
+	.asciz	"4902f3d8-07a9-403d-ad06-76b32dde12c4"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
