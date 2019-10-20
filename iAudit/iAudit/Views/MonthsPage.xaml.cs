52af9781df768fd2a948713033d68253046e4574
Sonia Lopez <sonlopez99@gmail.com>
2019-10-19 23:30:45 -0500

    Adding Month functionality

--- iAudit/iAudit/Views/MonthsPage.xaml.cs
+++ iAudit/iAudit/Views/MonthsPage.xaml.cs
@@ -7,6 +7,7 @@ namespace iAudit.Views
 {
     public partial class MonthsPage : ContentPage
     {
+        Year currentYear;
         public MonthsPage()
         {
             InitializeComponent();
@@ -15,6 +16,73 @@ namespace iAudit.Views
         public MonthsPage(Year year)
         {
             InitializeComponent();
+            currentYear = year;
+        } 
+
+        async void Jan_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear,"January"));
+        }
+
+        async void Feb_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear, "February"));
+        }
+
+        async void Mar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear, "March"));
+        }
+
+        async void Apr_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear, "April"));
+        }
+
+        async void May_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear, "May"));
+        }
+
+        async void Jun_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear, "June"));
+        }
+
+        async void Jul_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear, "July"));
+        }
+
+
+        async void Aug_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear, "August"));
+        }
+
+        async void Sep_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear, "September"));
+        }
+
+        async void Oct_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear, "October"));
+        }
+
+        async void Nov_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear, "November"));
+        }
+
+        async void Dec_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear, "December"));
+        }
+
+        async void All_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SelectPage(currentYear));
         }
     }
 }
---
+++ iAudit/iAudit/Views/SelectPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using iAudit.Models;
+using Xamarin.Forms;
+
+namespace iAudit.Views
+{
+    public partial class SelectPage : ContentPage
+    {
+        public SelectPage()
+        {
+            InitializeComponent();
+        }
+
+        public SelectPage(Year year, String month)
+        {
+            InitializeComponent();
+        }
+
+        public SelectPage(Year year)
+        {
+            InitializeComponent();
+        }
+    }
+}
