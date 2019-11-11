3600030bfa33883ee09c7e649c14293f920dd074
Sonia Lopez <sonlopez99@gmail.com>
2019-11-11 01:22:49 -0600

    Report Update

--- iAudit/iAudit/Views/ViewReportPage.xaml.cs
+++ iAudit/iAudit/Views/ViewReportPage.xaml.cs
@@ -11,7 +11,11 @@ namespace iAudit.Views
     
     public partial class ViewReportPage : ContentPage
     {
-        
+        public double Income { get; set; }
+        public double Expense { get; set; }
+        public double Profit_Loss { get; set; }
+        public double Projection{ get; set; }
+
         public ViewReportPage()
         {
             InitializeComponent();
@@ -58,6 +62,66 @@ namespace iAudit.Views
             var chart5 = new RadarChart() { Entries = entries };
             */
             this.chartView.Chart = chart;
+
+            var entries1 = new[]
+            {
+                new Microcharts.Entry(1000)
+                {
+                    Label = "January",
+                    ValueLabel = "200",
+                    Color = SKColor.Parse("#104950")
+                    //FillColor = SKColor.Parse("#266489")
+                },
+                new Microcharts.Entry(4020)
+                {
+                    Label = "February",
+                    ValueLabel = "400",
+                    Color = SKColor.Parse("#F7A4B9")
+                },
+                new Microcharts.Entry(-100)
+                {
+                    Label = "March",
+                    ValueLabel = "-100",
+                    Color = SKColor.Parse("#0084b4")
+                }
+            };
+
+            var line = new LineChart() { Entries = entries1 };
+
+            this.lineView.Chart = line;
+
+            var entries2 = new[]
+            {
+                new Microcharts.Entry(1000)
+                {
+                    Label = "January",
+                    ValueLabel = "200",
+                    Color = SKColor.Parse("#104950")
+                    //FillColor = SKColor.Parse("#266489")
+                },
+                new Microcharts.Entry(4020)
+                {
+                    Label = "February",
+                    ValueLabel = "400",
+                    Color = SKColor.Parse("#F7A4B9")
+                },
+                new Microcharts.Entry(-100)
+                {
+                    Label = "March",
+                    ValueLabel = "-100",
+                    Color = SKColor.Parse("#0084b4")
+                }
+            };
+
+            var donut = new DonutChart() { Entries = entries2 };
+
+            this.donutView.Chart = donut;
+
+            Income = 19000;
+            Expense = 1200;
+            Profit_Loss = Income - Expense;
+            Projection = Profit_Loss / Income * 100;
+            
         }
         public ViewReportPage(Year year)
         {
