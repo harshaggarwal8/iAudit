0ab06a683e25bec26b9dbbe9ab5b00e7b4ab4a06
harshaggarwal8 <officialharshagg8@gmail.com>
2019-11-30 16:40:17 -0600

    Email class added

---
+++ iAudit/iAudit/Views/EmailTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace iAudit.Models
+{
+    public class EmailTest
+    {
+        public EmailTest()
+        {
+        }
+        public async Task SendEmail(string subject, string body, List<string> recipients)
+        {
+            try
+            {
+                var message = new EmailMessage
+                {
+                    Subject = subject,
+                    Body = body,
+                    To = recipients,
+                    //Cc = ccRecipients,
+                    //Bcc = bccRecipients
+                };
+
+                var fn = "Attachment.txt";
+                var file = Path.Combine(FileSystem.CacheDirectory, fn);
+                File.WriteAllText(file, "Hello World");
+
+                message.Attachments.Add(new EmailAttachment(file));
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                // Email is not supported on this device
+            }
+            catch (Exception ex)
+            {
+                // Some other exception occurred
+            }
+        }
+    }
+}
